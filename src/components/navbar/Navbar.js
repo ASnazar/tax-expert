f460a18cf4f776fcca4ee1aeba53fb6d5b6c97c9
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-03-07 11:54:37 +0200

    Init_10

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () =>{
                         <div className={style.logo_image_menu}>
                             <img src={Logo_2} alt="/"/>
                             </div>
-                        <li ><a href='/home'>Послуги</a> </li>
+                        <li ><a href='/hom'>Послуги</a> </li>
                         <li ><a href='/home'>Про нас</a> </li>
                         <li ><a href='/home'>Ціни</a> </li>
                         <li ><a href='/home'>Контакти</a> </li>
