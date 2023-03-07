db859aaf719b64c5cf807a7daa0eddebc7e1177d
ASnazar <36624661+ASnazar@users.noreply.github.com>
2023-03-07 02:07:26 +0200

    Init_4

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,7 @@
 import React, {useState} from 'react';
 import style from './Navbar.module.css';
-import Logo from '../../asserts/logo.png'
-import Logo_2 from '../../asserts/logo_2.png'
-import Logo_head from '../../asserts/logo_burg.png'
+import Logo_2 from '../../asserts/logo_2.png';
 import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
-import {MdOutlinePhoneInTalk} from "react-icons/md";
 const Navbar = () =>{
     const [nav, setNav] = useState(false);
     return (
@@ -18,10 +15,10 @@ const Navbar = () =>{
                         <div className={style.logo_image_menu}>
                             <img src={Logo_2} alt="/"/>
                             </div>
-                        <li ><a href='##'>Послуги</a> </li>
-                        <li ><a href='##'>Про нас</a> </li>
-                        <li ><a href='##'>Ціни</a> </li>
-                        <li ><a href='##'>Контакти</a> </li>
+                        <li ><a href='#'>Послуги</a> </li>
+                        <li ><a href='#'>Про нас</a> </li>
+                        <li ><a href='#'>Ціни</a> </li>
+                        <li ><a href='#'>Контакти</a> </li>
 
                     </ul>
                     <div onClick={()=>setNav(!nav)} className={style.mobile_btn}>
